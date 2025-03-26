dc923ffc54bdae16520313c0be4b7ddba93f6095
Kauan Fank <kauan.fank@gmail.com>
2025-03-26 13:26:13 -0300

    .

--- vjudge/21032025/d.cpp
+++ vjudge/21032025/d.cpp
@@ -4,89 +4,122 @@
 using namespace std;
 
 vector<int> primes;
-bitset<1000000> bs;
-
+bitset<1000006> bs;
+vector<vector<int>> fatores(1000005);
+vector<int> pot2(100001);
 void sieve(){
 	bs.set();
-	for(int i = 2; i*i < 1000000; i++){
-		if(bs[i]) primes.push_back(i);
-		for(int p = i*i; p < 1000000; p+=i){
+	for(int i = 2; i*i <= 1000005; i++){
+		if(bs[i]){
+			primes.push_back(i);
+			fatores[i].push_back(i);
+		} else continue;
+		for(int p = i*2; p <= 1000005; p+=i){
 			bs.reset(p);
+			fatores[p].push_back(i);
+		}
+	}
+	for(int i = 1000; i<1000005; i++){
+		if(bs[i]){
+			fatores[i].push_back(i);
+			for(int p = i*2; p <= 1000005; p+=i){
+				bs.reset(p);
+				fatores[p].push_back(i);
+			}
 		}
 	}
 }
 
-void factors(int b, vector<int> &num_factors){
-	if(b == 1) return;
-	for(int e: primes){
-		if(b % e == 0){
-			num_factors.push_back(e);
-			do {
-				b /= e;
-			}while(b && b%e == 0);
-		}
-		if(b == 0) break;
-	}
-	if(b > 1) {
-		num_factors.push_back(b);
-	}
-}
+// void factors(int b, vector<int> &num_factors){
+// 	if(b == 1) return;
+// 	for(int e: primes){
+// 		if(b % e == 0){
+// 			num_factors.push_back(e);
+// 			do {
+// 				b /= e;
+// 			}while(b && b%e == 0);
+// 		}
+// 		if(b == 0) break;
+// 	}
+// 	if(b > 1) {
+// 		num_factors.push_back(b);
+// 	}
+// }
 
-int pow_mod(int a, int n){
-	int res = 1;
-	while(n){
-		if(n & 1){
-			res = (res*a)%md;
-		}
-		a = (a*a)%md;
-		n >>=1;
-	}
-	return res;
-}
+// int mult_mod(int a, int b){
+// 	int res = 0;
+// 	while(b){
+// 		if(b & 1){
+// 			res = (res+a)%md;
+// 		}
+// 		a = (a+a)%md;
+// 		b >>= 1;
+// 	}
+// 	return res;
+// }
+
+// int pow_mod(int a, int b){
+// 	int res = 1;
+// 	while(b){
+// 		if(b & 1){
+// 			res = (mult_mod(a, b))%md;
+// 		}
+// 		a = (mult_mod(a, a))%md;
+// 		b >>=1;
+// 	}
+// 	return res;
+// }
 
 signed main(){
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	//cout.tie(0);
 	sieve();
-	int a; cin>>a;
-	unordered_map<int, unordered_map<int, int>> conflict;
-	// Vou mapear os conflitos entre os primos, caso um numero possua os
-	// fatores 2 * 3 * 7, vou mapear todo mundo com todo mundo, isto é
-	// m[2][3]++, m[2][7]++, m[3][2]++, m[3][7]++ etc... Como um número até 10^6 possui
-	// no máximo 7 fatores primos distintos, a complexidade será 7*7*10^5, o que é viável
+	pot2[0] = 1;
+	for(int i = 1; i<100001; i++){
+		pot2[i] = (pot2[i-1]*2)%md;
+	}
 
-	// Além disso, vou obviamente salvar a quantidade de números da entrada que
-	// possuem cada determinado primo. Dessa forma, eu irei considerar estes, e
-	// depois irei subtrair cada conflito (da forma que eu fiz, os conflitos estão
-	// duplicados).
-	unordered_map<int, int> prime_count;
+	// for(int i = 0; i<100; i++){
+	// 	cout<<i<<":";
+	// 	for(int e: fatores[i]) cout<<" "<<e;
+	// 	cout<<endl;
+	// }
+	int a; cin>>a;
+	vector<int> m(1000001, 0);
 	for(int i= 0; i<a; i++){
 		int b; cin>>b;
-		vector<int> num_factors;
-		factors(b, num_factors);
-		for(int j = 0; j<num_factors.size(); j++){
-			prime_count[num_factors[j]]++;
-			for(int k = j+1; k<num_factors.size(); k++){
-				conflict[num_factors[j]][num_factors[k]]++;
+		int n = fatores[b].size();
+
+		for(int i = 1; i<(1<<n); i++){
+			int res = 1;
+			for(int j = 0; j<n; j++){
+				if(i & (1<<j)) res*= fatores[b][j];
 			}
+			m[res]++;
 		}
 	}
 	int q; cin>>q;
 	for(int i = 0; i<q; i++){
 		int b; cin>>b;
-		vector<int> num_factors;
-		factors(b, num_factors);
 		int sum = 0;
-		for(int e: num_factors){
-			sum += prime_count[e];
-		}
-		for(int j = 0; j<num_factors.size(); j++){
-			for(int k = j+1; k<num_factors.size(); k++){
-				sum -= conflict[num_factors[j]][num_factors[k]];
+		int n = fatores[b].size();
+		for(int i = 1; i<(1<<n); i++){
+			int res = 1;
+			int count = 0;
+			for(int j = 0; j<n; j++){
+				if(i & (1<<j)) {
+					res*=fatores[b][j];
+					count++;
+				}
+			}
+			if(count & 1){
+				sum += m[res];
+			} else {
+				sum -= m[res];
 			}
 		}
-		int res = pow_mod(2, a - sum);
+		int res = pot2[a - sum];
 		cout<<res<<endl;
 	}
 }
