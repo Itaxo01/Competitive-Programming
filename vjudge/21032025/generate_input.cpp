3d6dc8bbd6b23389ab8424803682863cc5b8f281
Kauan Fank <kauan.fank@gmail.com>
2025-03-25 22:42:21 -0300

    update

--- vjudge/21032025/generate_input.cpp
+++ vjudge/21032025/generate_input.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(0);
-	cout.tie(0);
-	int n = 1e5;
-	int q = 1e5;
-	int v = 1e6;
-	cout<<n<<endl;
-	for(int i = 1; i<=n; i++){
-		cout<<i<<" ";
-	}
-	cout<<q<<endl;
-	for(int i = 1; i<=q; i++){
-		cout<<rand()%v<<endl;
+    ios_base::sync_with_stdio(0);
+    cout.tie(0);
+    int n = 1e5;
+    int q = 1e5;
+    int v = 1e6;
+    cout << n << endl;
+    for(int i = 1; i <= n; i++){
+        cout << i << " ";
+    }
+    cout << q << endl;
+    
+    random_device rd;
+    mt19937 rng(rd());  // Mersenne Twister RNG
+    uniform_int_distribution<int> dist(0, v-1);
+
+    for(int i = 1; i <= q; i++){
+        cout << dist(rng) <<endl;
 	}
 }
