a12037d4c7d524d0412638a029719344c8a7a419
Kauan Fank <kauan.fank@gmail.com>
2025-08-25 19:48:22 -0300

    a

--- vjudge/topicos2denovo/sim1/d.cpp
+++ vjudge/topicos2denovo/sim1/d.cpp
@@ -7,17 +7,17 @@ int main(){
 	// ela só precisará esperar até outro dia para uma consulta se houver mais de um médico naquele dia.
 	// no pior dos casos, ela terá que esperar 1e6 dias, pois se todos começarem no dia 1000 e terem o mesmo ciclo de 1000, ela visitará um a cada 1000 dias totalizando os 1e6. 
 
+	vector<bool> v;
 	bitset<1001006> bs;
 	bs.reset();
 	int menor = 0;
 
 	vector<pair<int, int>> d(n);
-	for(auto &[a, b]: d) cin>>a>>b;
-
 	for(auto &[a, b]: d){ 
 		// aqui vou só simular qual o primeiro livre.
+		cin>>a>>b;
 		while(bs[a] || a < menor) a+=b;
-		menor = max(menor, a);
+		menor = a;
 		bs.set(a);
 	}
 	cout<<menor<<endl;
